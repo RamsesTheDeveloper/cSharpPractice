45241e2b31fafa1c51ff6ef9a72ad5bb5a718026
Ramsés Abdala <change@Ramsess-MacBook-Pro.local>
2022-07-29 10:55:52 -0500

    first commit

---
+++ cSharp/cSharp/Program.cs
@@ -0,0 +1,37 @@
+﻿// See https://aka.ms/new-console-template for more information
+// Console.WriteLine("Hello, World!");
+
+//namespace HelloWorld
+//{
+//    class Program
+//    {
+//        static void Main(string[] args)
+//        {
+//            // print to the console
+//            // Console.WriteLine("Hello, World");
+//            // Console.ReadLine();
+
+//            // data types
+//            //int x;
+//            //int y;
+
+//            //x = 7;
+//            //y = x + 3;
+
+//            //Console.WriteLine(y);
+//            //Console.ReadLine();
+
+//            Console.WriteLine("What is your name?");
+//            Console.Write("Type your first name: ");
+//            string myFirstName;
+//            myFirstName = Console.ReadLine();
+
+//            string myLastName;
+//            Console.Write("Type your last name: ");
+//            myLastName = Console.ReadLine();
+
+//            Console.WriteLine("Hello, " + myFirstName + " " + myLastName);
+
+//        }
+//    }
+//}
